82c58cec48b13cae82274c9a7d43901ff806bf86
yu3589 <uegtz34714@yahoo.co.jp>
2025-07-12 17:22:12 +0900

    fix :静的OGPが表示されない問題を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
+  before_action :set_default_meta
+
+  private
+
+  def set_default_meta
+    set_meta_tags(default_meta_tags)
+  end
 end
